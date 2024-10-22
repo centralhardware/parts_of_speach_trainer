a27aae508804387639adc540b8ef292748d1701d
Fedechkin Aleksei <alex@centralhardware.me>
2024-10-22 11:36:26 +0200

    increase statistic session finish timeout

--- src/main/kotlin/Statistic.kt
+++ src/main/kotlin/Statistic.kt
@@ -5,13 +5,13 @@ import korlibs.time.minutes
 
 object Statistic {
 
-    val cache = TimedList<Long, String, Boolean>(1.minutes)
+    val cache = TimedList<Long, String, Boolean>(5.minutes)
 
     suspend fun add(user: User, word: String, correct: Boolean) {
         cache.add(user.rawId(), word, correct)
     }
 
-    suspend fun getStatistic(user: ChatId): Pair<Int, Int> {
+    fun getStatistic(user: ChatId): Pair<Int, Int> {
         val stat = cache.getElementsByKey1(user.rawId()).groupBy{ it.first }.mapValues { entry ->
             entry.value.any{ !it.second }
         }
