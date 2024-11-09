4aaa75a6c931383ce8b9f3e3c66c11ecc1e31a35
Fedechkin Aleksei <alex@centralhardware.me>
2024-11-09 17:43:44 +0100

    bump commons

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 
 dependencies {
     implementation("dev.inmo:tgbotapi:20.0.0")
-    implementation("com.github.centralhardware:telegram-bot-commons:ef17c6cc90")
+    implementation("com.github.centralhardware:telegram-bot-commons:d96b131958")
     implementation("com.github.seratch:kotliquery:1.9.0")
     implementation("org.postgresql:postgresql:42.7.4")
     implementation("io.github.crackthecodeabhi:kreds:0.9.1")
